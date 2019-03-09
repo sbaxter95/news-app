ac89f8a2f9862ef9cb3aeacfb907b3fe380fc179
Sophie Baxter <sbaxter95@outlook.com>
2019-03-09 18:40:03 +0000

    🎨 - refactoring topic card component

--- src/components/Card/TopicCard.js
+++ src/components/Card/TopicCard.js
@@ -2,15 +2,33 @@ import React from 'react';
 import './styles.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const TopicCard = props => {
-  return (
-    <div className="topic-card">
-      <h3>{props.topic}</h3>
-      <div className="icon-container">
-        <FontAwesomeIcon icon={props.icon} size="3x" />
+class TopicCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selected: false };
+  }
+
+  toggleClass = () => {
+    const isSelected = this.state.selected;
+    this.setState({ selected: !isSelected });
+  };
+
+  render() {
+    let className = 'topic-card';
+    if (this.state.selected) {
+      className = 'topic-card-selected';
+    } else {
+      className = 'topic-card';
+    }
+    return (
+      <div className={className} onClick={this.toggleClass}>
+        <h3>{this.props.topic}</h3>
+        <div className="icon-container">
+          <FontAwesomeIcon icon={this.props.icon} size="3x" />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default TopicCard;
