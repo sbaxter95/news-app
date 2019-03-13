28e00522846ed578cfc0bbda3ec5897d37c6e87b
Sophie Baxter <sbaxter95@outlook.com>
2019-03-13 21:51:42 +0000

    ✨ - added basic router and started to add some pages

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
-import TopicCard from './components/Card/TopicCard';
-import Button from './components/Button/Button';
-import Grid from './components/Grid/Grid';
-import ButtonContainer from './components/ButtonContainer/ButtonContainer';
-
-import { faBullhorn } from '@fortawesome/free-solid-svg-icons';
-import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
-import { faHeartbeat } from '@fortawesome/free-solid-svg-icons';
-import { faMusic } from '@fortawesome/free-solid-svg-icons';
-import { faFootballBall } from '@fortawesome/free-solid-svg-icons';
-import { faMicroscope } from '@fortawesome/free-solid-svg-icons';
-import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import Topics from './pages/Topics';
+import Home from './pages/Home';
+import Languages from './pages/Languages';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>News App</h1>
-        <h2>What topics are you interested in?</h2>
-        <Grid>
-          <TopicCard topic="Politics" icon={faBullhorn} />
-          <TopicCard topic="Business" icon={faBriefcase} />
-          <TopicCard topic="Health" icon={faHeartbeat} />
-        </Grid>
-        <Grid>
-          <TopicCard topic="General" icon={faNewspaper} />
-          <TopicCard topic="Entertainment" icon={faMusic} />
-          <TopicCard topic="Science" icon={faMicroscope} />
-        </Grid>
-        <Grid>
-          <TopicCard topic="Sports" icon={faFootballBall} />
-          <TopicCard topic="Technology" icon={faLaptopCode} />
-          <TopicCard topic="None" icon={faTimes} />
-        </Grid>
-        <ButtonContainer>
-          <Button buttonText="Continue" />
-        </ButtonContainer>
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/topics" component={Topics} />
+            <Route path="/languages" component={Languages} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,7 @@ import './styles.css';
 
 const Button = props => {
   return (
-    <div className="button-container">
+    <div className="button-container" onClick={props.onClick}>
       <h3>{props.buttonText}</h3>
     </div>
   );
